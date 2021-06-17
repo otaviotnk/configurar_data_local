ad27b6a6f94f02285712891cdeb8868121c2dad2
Otavio Rockenbach <otavio.atr@gmail.com>
2021-06-16 23:36:25 -0300

    first commit

---
+++ ConfigureLocalTime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TimeZoneConverter;
+
+namespace ConfigureLocalTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Necessita do pacote TimeZoneConverter
+
+            var defaultDate = DateTime.UtcNow; //.Now
+
+            var brazilianDate = GetLocalDate(defaultDate);
+
+            Console.WriteLine("Data Servidor");
+            Console.WriteLine(defaultDate);
+            Console.WriteLine("-----------");
+            Console.WriteLine("Data Local");
+            Console.WriteLine(brazilianDate);
+        }
+
+        private static DateTime GetLocalDate(DateTime date)
+        {
+            var dateUtc = DateTime.SpecifyKind(date, DateTimeKind.Utc);
+            var timeZoneInfo = TZConvert.GetTimeZoneInfo("Brazil/east");
+            var localDate = TimeZoneInfo.ConvertTime(dateUtc, timeZoneInfo);
+
+            return localDate;
+        }
+    }
+}
